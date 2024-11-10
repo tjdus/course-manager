8acf761491efbdcb41b054e2f559a96a7b158d11
tjdus <dusml0277@gmail.com>
2024-11-10 23:48:05 +0900

    fix

--- core/serializers/professor.py
+++ core/serializers/professor.py
@@ -6,31 +6,22 @@ from core.serializers.department import DepartmentSerializer
 
 
 class ProfessorSerializer(serializers.ModelSerializer):
-    department = DepartmentSerializer()
+    department = DepartmentSerializer(read_only=True)
+    department_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Department.objects.all())
     class Meta:
         model = Professor
         fields = '__all__'
 
     def create(self, validated_data):
-        department_data = validated_data.pop('department')
-
-        try:
-            department = Department.objects.get(**department_data)
-        except ObjectDoesNotExist:
-            raise ValidationError("Department not found with the provided data")
-
-        professor = Professor.objects.create(department=department, **validated_data)
+        department_data = validated_data.pop('department_id')
+        professor = Professor.objects.create(department=department_data, **validated_data)
         return professor
 
     def update(self, instance, validated_data):
-        department_data = validated_data.pop('department', None)
+        department_data = validated_data.pop('department_id')
 
         if department_data:
-            try:
-                department = Department.objects.get(**department_data)
-                instance.department = department
-            except ObjectDoesNotExist:
-                raise ValidationError("Department not found with the provided data")
+            instance.department = department_data
 
         instance.employee_id = validated_data.get('employee_id', instance.employee_id)
         instance.save()
