9d3f18575f7bb576823778804f5ae6510d6f45c9
tjdus <dusml0277@gmail.com>
2024-11-18 13:30:38 +0900

    answer

--- core/serializers/enrollment.py
+++ core/serializers/enrollment.py
@@ -1,6 +1,18 @@
 from rest_framework import serializers
 
+from core.models.enrollment import Enrollment
+from core.serializers.course import CourseSerializer
+from core.serializers.student import StudentSerializer
+
 
 class EnrollmentSerializer(serializers.ModelSerializer):
-    #TODO:
-    pass
+    course = CourseSerializer(read_only=True)
+    student = StudentSerializer(read_only=True)
+
+    class Meta:
+        model = Enrollment
+        fields = ['id', 'course', 'student']
+
+
+class EnrollmentRequestSerializer(serializers.Serializer):
+    course_id = serializers.IntegerField(required=True)
