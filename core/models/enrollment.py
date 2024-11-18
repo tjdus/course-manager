700463e0e720055fc8c52c7b3ad9c2cbf1159461
tjdus <dusml0277@gmail.com>
2024-11-18 13:30:38 +0900

    feat: add enrollment

--- core/models/enrollment.py
+++ core/models/enrollment.py
@@ -2,5 +2,14 @@ from django.db import models
 
 
 class Enrollment(models.Model):
-    #TODO:
-    pass
+    student = models.ForeignKey('core.Student', on_delete=models.PROTECT)
+    course = models.ForeignKey('core.Course', on_delete=models.PROTECT)
+    enroll_date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'enrollment'
+        verbose_name = '수강 신청'
+        constraints = [models.UniqueConstraint(
+            fields=['student', 'course'],
+            name='unique_student_course'
+        )]
